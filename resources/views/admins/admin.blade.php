f5079e7919ae39669d3d7e2f44577c0e892558ff
emiln <emil.nedregard@gmail.com>
2017-02-21 12:35:57 +0100

    content er innenfor sitt område

--- resources/views/admins/admin.blade.php
+++ resources/views/admins/admin.blade.php
@@ -4,7 +4,6 @@
 
 @section('content')
 
-        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
           <h1 class="page-header">Dashboard</h1>
 
 
@@ -33,7 +32,6 @@
           </div>
 
         
-        </div>
       </div>
     </div>
 
