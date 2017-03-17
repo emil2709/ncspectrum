5550b97564904a1c551e09e1f085056f53f28c26
emiln <emil.nedregard@gmail.com>
2017-03-17 12:02:57 +0100

    Endringer i log, ser like bra ut som resten av siden.

--- resources/views/admins/log.blade.php
+++ resources/views/admins/log.blade.php
@@ -5,7 +5,12 @@
 @section('content')
 
 <h1>Log</h1>
-<table id="myTable" class="tablesorter">
+<table class="table table-striped">
+		<div class="input-group margin-bottom">        
+	        <span class="input-group-addon" id="search-addon"><i class="glyphicon glyphicon-search"></i></span>
+	        <input type="text" id="search" class="form-control" placeholder="Search ...">
+	        <input type="hidden" id="type" value="guests">
+      	</div>
 	<thead>
 		<tr>
 			<th>Fistname</th>
@@ -15,7 +20,7 @@
 			<th>To</th>
 		</tr>
 	</thead>
-	<tbody>
+	<tbody id="searchresult">
 		<tr>
 			@foreach ($users as $user)
 			@foreach ($user->visits as $visit)
