3d0a093bf9e5bbcd4428d4bc6a9766f2e09c278b
emiln <emil.nedregard@gmail.com>
2017-04-03 23:19:03 +0200

    med knapper og sortering

--- resources/views/admins/status.blade.php
+++ resources/views/admins/status.blade.php
@@ -4,6 +4,39 @@
 
 @section ('content')
 
+<script type="text/javascript">
+        var people, asc1 = 1,
+            asc2 = 1,
+            asc3 = 1;
+        window.onload = function () {
+            people = document.getElementById("people");
+        }
+
+        function sort_table(tbody, col, asc) {
+            var rows = tbody.rows,
+                rlen = rows.length,
+                arr = new Array(),
+                i, j, cells, clen;
+            // fill the array with values from the table
+            for (i = 0; i < rlen; i++) {
+                cells = rows[i].cells;
+                clen = cells.length;
+                arr[i] = new Array();
+                for (j = 0; j < clen; j++) {
+                    arr[i][j] = cells[j].innerHTML;
+                }
+            }
+            // sort the array by the specified column number (col) and order (asc)
+            arr.sort(function (a, b) {
+                return (a[col] == b[col]) ? 0 : ((a[col] > b[col]) ? asc : -1 * asc);
+            });
+            // replace existing rows with new rows created from the sorted array
+            for (i = 0; i < rlen; i++) {
+                rows[i].innerHTML = "<td>" + arr[i].join("</td><td>") + "</td>";
+            }
+        }
+    </script>
+
 <h2 class="sub-header">Status</h2>
 
   <div class="table-responsive">
@@ -17,20 +50,28 @@
 
       <thead>
       	<tr>
-      		<th>Firstname</th>
-      		<th>Lastname</th>
-      		<th>Status</th>
+      		<th onclick="sort_table(people, 0, asc1); asc1 *= -1; asc2 = 1; asc3 = 1;">Firstname
+      		<span class="glyphicon glyphicon-arrow-down"></span></th>
+            <th onclick="sort_table(people, 1, asc2); asc2 *= -1; asc3 = 1; asc1 = 1;">Lastname
+            <span class="glyphicon glyphicon-arrow-down"></span></th>
+            <th onclick="sort_table(people, 2, asc3); asc3 *= -1; asc1 = 1; asc2 = 1;">Status
+            <span class="glyphicon glyphicon-arrow-down"></span></th>
+            <th>Change</th>
       	</tr>
       </thead>
-      <tbody id="searchresult">
+      <tbody id="people">
       	<tr>
       	@foreach ($users as $user)
 		    <td>{{ $user->firstname }} </td>
 		    <td>{{ $user->lastname }}</td>
 		    <td>
 		        @if ($user->status)
-		            {{ $user->status->status }}
+					{{ $user->status->status }}
 		        @endif
+    		</td>
+    		<td>
+    			<button type="button" class="btn btn-primary">Default</button>
+
     		</td>
 		</tr>
 @endforeach
