a188b38407d5460a4275521723836db25ec5b5ba
emiln <emil.nedregard@gmail.com>
2017-04-04 11:17:06 +0200

    knappens front end kan endres

--- resources/views/admins/status.blade.php
+++ resources/views/admins/status.blade.php
@@ -4,38 +4,6 @@
 
 @section ('content')
 
-<script type="text/javascript">
-        var people, asc1 = 1,
-            asc2 = 1,
-            asc3 = 1;
-        window.onload = function () {
-            people = document.getElementById("people");
-        }
-
-        function sort_table(tbody, col, asc) {
-            var rows = tbody.rows,
-                rlen = rows.length,
-                arr = new Array(),
-                i, j, cells, clen;
-            // fill the array with values from the table
-            for (i = 0; i < rlen; i++) {
-                cells = rows[i].cells;
-                clen = cells.length;
-                arr[i] = new Array();
-                for (j = 0; j < clen; j++) {
-                    arr[i][j] = cells[j].innerHTML;
-                }
-            }
-            // sort the array by the specified column number (col) and order (asc)
-            arr.sort(function (a, b) {
-                return (a[col] == b[col]) ? 0 : ((a[col] > b[col]) ? asc : -1 * asc);
-            });
-            // replace existing rows with new rows created from the sorted array
-            for (i = 0; i < rlen; i++) {
-                rows[i].innerHTML = "<td>" + arr[i].join("</td><td>") + "</td>";
-            }
-        }
-    </script>
 
 <h2 class="sub-header">Status</h2>
 
@@ -70,8 +38,9 @@
 		        @endif
     		</td>
     		<td>
-    		 <a href="{{ action('AdminController@update/{$user->status->id}/') }}"</a>
-
+          <a href="{{ URL::route('admins.changeStatus', [$user->id]) }}" title="Change Status">
+                <button class="btn btn-primary">Change status</button>
+              </a>
     		</td>
 		</tr>
 @endforeach
